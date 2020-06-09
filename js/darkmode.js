0c63364be291fa7e9cfff4b372473120281fdd34
cedric.chaffard@gmail.com <cedric.chaffard@gmail.com>
2020-06-09 10:18:55 +0200

    Operationnal Dark mode  - Improvments & layout corrections (bloc-4-portfolio centered - Bloc-8, etc)

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,5 @@
 const btnToggle = document.querySelector(".btn-toggle");
 
-
 btnToggle.addEventListener("click", () =>{
 
     const body = document.body;
@@ -10,28 +9,44 @@ btnToggle.addEventListener("click", () =>{
         body.classList.add("light")
         body.classList.remove("dark")
 
-
-
     }else if(body.classList.contains("light")){
 
         body.classList.add("dark")
         body.classList.remove("light")
-
-
-
 
     }
 
     //LCA LOGO IMG SWITCH
 
     if($('body').hasClass('light'))
+
     {
         $('.img-clair').attr('src','img/la-chouette-agence.png');
         $('.logo-clair').attr('src','img/logo.png');
 
     }else
+
     {
         $('.img-clair').attr('src','img/la-chouette-agence_white.png');
         $('.logo-clair').attr('src','img/logo copie.png');
+
+    }
+
+})
+
+const darkModeWord = document.querySelector(".dark-mode");
+
+btnToggle.addEventListener("click", () =>{
+
+    const body = document.body;
+
+    if(body.classList.contains("dark")){
+
+        darkModeWord.innerHTML = "Mode clair"
+
+    }else if(body.classList.contains("light")){
+
+        darkModeWord.innerHTML = "Mode sombre"
+
     }
 })
